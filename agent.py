a1863284bb366072398427cdba189d260b9944af
OndraNeveril <ondra.neveril@centrum.cz>
2025-10-13 20:16:42 +0200

    fix from stage

--- agent.py
+++ agent.py
@@ -3,11 +3,11 @@ from random import randint
 
 class Agent_1(Agent):
     def k_v_listu(self, k, list_dices):
-        m = [0 for j in range(6)]
-        for j in range(1, 7):
+        m = [0 for i in range(6)]
+        for i in range(1, 7):
             while True:
-                if j in list_dices:
-                    m[j - 1] += 1
+                if i in list_dices:
+                    m[i - 1] += 1
                     list_dices.remove(j)
                 else:
                     break
@@ -16,31 +16,34 @@ class Agent_1(Agent):
             else:
                 return None, None
 
-    def jedna_v_n(n, k):
-        p = 0
-        for i in range(1000000):
-            m = [0, 0]
-            l = [randint(1, 6) for j in range(n)]
-            while True:
-                if 1 in l:
-                    m[0] += 1
-                    l.remove(1)
-                else:
-                    break
-            if max(m) >= k:
-                p += 1
-        return p / 1000000
+    def obsahuje_k(self,k,list_dices):
+        for x in list_dices:
+            if k == x:
+                return True
+        return False
 
-    def decide(self, score_total, score, list_dices, fails):
-        Next = None
+    def decide(self, score, score_total, list_dices, fails):
+        Next = False
         list_back = []
-        if list_dices.sort() == [i for i in range(1, 7)]:
+        """if list_dices.sort() == [i for i in range(1, 7)]:
+            print('a')
             list_back = list_dices
+            Next = True"""
+        if 1 in list_dices:
+            list_back = [1]
             Next = True
-        elif self.k_v_listu(len(list_dices), list_dices) != None:
-            list_back = list_dices
+        elif 5 in list_dices:
+            list_back = [5]
             Next = True
-        else:
-            Next = False
+        if score >= 350:
+            return [False, list_back]
 
-        return Next, list_back
+        return [Next, list_back]
+
+agent = Agent_1()
+env = Environment()
+
+score = 0
+for i in range(1000):
+    score += env.play_one_turn(agent, 0, 0)
+print(score/1000)
