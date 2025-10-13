6ed4e4c37532d5a2c53cd039fbc66d19fdc2fd7c
OndraNeveril <ondra.neveril@centrum.cz>
2025-10-13 20:30:28 +0200

    kolik obsahuje k?

--- agent.py
+++ agent.py
@@ -2,33 +2,24 @@ from environment import Environment, Agent
 from random import randint
 
 class Agent_1(Agent):
-    def k_v_listu(self, k, list_dices):
-        m = [0 for i in range(6)]
-        for i in range(1, 7):
-            while True:
-                if i in list_dices:
-                    m[i - 1] += 1
-                    list_dices.remove(j)
-                else:
-                    break
-            if max(m) >= k:
-                return m.index(max(m)) + 1, max(m)
-            else:
-                return None, None
 
-    def obsahuje_k(self,k,list_dices):
-        if k in list_dices:
-            return True
-        else:
-            return False
+    def kolik_obsahuje_k(self,k,list_dices):
+        count = 0
+        while True:
+            if k in list_dices:
+                count += 1
+                list_dices.remove(k)
+            else:
+                return count
+
 
     def decide(self, score, score_total, list_dices, fails):
         Next = False
         list_back = []
-        """if list_dices.sort() == [i for i in range(1, 7)]:
+        if list_dices.sort() == [i for i in range(1, 7)]:
             print('a')
             list_back = list_dices
-            Next = True"""
+            Next = True
         if 1 in list_dices:
             list_back = [1]
             Next = True
