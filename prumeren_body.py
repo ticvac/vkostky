67bca6e9ccc1acaf64bcb9856f000a0789673114
jindrichkaplikcky <jindrich.kaplicky@gmail.com>
2025-10-13 20:47:06 +0200

    Pravdepodobnostis

---
+++ prumeren_body.py
@@ -0,0 +1,23 @@
+from environment import Environment
+from random import randint
+
+nuly = 0
+soucet = 0
+
+env = Environment()
+
+kostek = 5
+iteraci = 100000
+
+for i in range(iteraci):
+    vs = [randint(1,6) for x in range(1,kostek + 1)]
+    #print(vs)
+    soucet += env.calculate_score(vs)
+    if env.calculate_score(vs) == 0:
+        nuly += 1
+
+print(kostek)
+print(soucet/iteraci)
+print(nuly/iteraci)
+
+
