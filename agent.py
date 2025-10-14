0852c5ad39eb0c5a4c5fcca6ac7ad8fb7dc7b7f7
jindrichkaplikcky <jindrich.kaplicky@gmail.com>
2025-10-15 01:17:23 +0200

    avg_turns a plot_graphs

--- agent.py
+++ agent.py
@@ -157,6 +157,25 @@ class Agent_5(Agent):
         # print(next_turn, list_back, list_dices)
         return [next_turn, list_back]
 
+def plot_graphs():
+    p = [agent_2, agent_3, agent_5]
+    names = ['agent_2', 'agent_3', 'agent_5']
+
+    list_of_scores = [env.play_one_game(p[x], 10000) for x in range(len(p))]
+
+    for i in range(len(list_of_scores)):
+        plt.plot(list_of_scores[i])
+        plt.savefig(names[i])
+        plt.close()
+
+def avg_turn(iterace = 1000):
+    p = [agent_2, agent_3, agent_5]
+
+    for i in p:
+        soucet = 0
+        for j in range(iterace):
+            soucet += len(env.play_one_game(i, 10000))
+        print("Soucet agent" , i , "  " , soucet/iterace)
 
 env = Environment()
 agent_0 = Agent_0()
@@ -166,12 +185,5 @@ agent_3 = Agent_3()
 agent_4 = Agent_4()
 agent_5 = Agent_5()
 
-p = [agent_2, agent_3, agent_5]
-names = ['agent_2', 'agent_3', 'agent_5']
 
-list_of_scores = [env.play_one_game(p[x], 10000) for x in range(len(p))]
-
-for i in range(len(list_of_scores)):
-    plt.plot(list_of_scores[i])
-    plt.savefig(names[i])
-    plt.close()
+avg_turn()
